27866ea250f5cb8b20998a163225cddd0108026b
choyalsandeep01 <65841671+choyalsandeep01@users.noreply.github.com>
2024-11-05 10:13:01 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from accounts.views import sign_up, log_in, activate_email,password_reset_request,password_reset_confirm
+from accounts.views import sign_up, log_in, activate_email,password_reset_request,password_reset_confirm,resend_email
 from home.views import home_view
 
 urlpatterns = [
@@ -10,4 +10,6 @@ urlpatterns = [
     path('<uuid:uuid>/', home_view, name='go_to_home'),
     path('login/password-reset/', password_reset_request, name='password_reset_request'),
     path('password-reset-confirm/<str:token>/', password_reset_confirm, name='password_reset_confirm'),
+    path('resend_email/', resend_email, name='resend_email'),
+
 ]
